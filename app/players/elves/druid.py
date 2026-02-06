6dfb654736fc99407d2bfec77ee329e3e50d4bf5
Rustam Mamatov <Mamatovr31@gmail.com>
2026-02-06 21:31:09 +0200

    fix mistake_1

--- app/players/elves/druid.py
+++ app/players/elves/druid.py
@@ -15,11 +15,11 @@ class Druid(Elf):
         self._favourite_spell = favourite_spell
 
     def player_info(self) -> str:
-            return (
+        return (
             f"Druid {self.nickname}. "
             f"{self.nickname} has a favourite "
             f"spell: {self._favourite_spell}"
-            )
+        )
 
     def get_rating(self) -> int:
         return len(self._favourite_spell)
